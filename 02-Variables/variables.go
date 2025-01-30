a7aa6e7a18fcf8a706144b00a2414de710825793
Abhijeet Sharan <abhijeetsharan0007@gmail.com>
2025-01-31 03:15:59 +0530

    Add Go modules and enhance variable examples in introductory files

--- 02-Variables/variables.go
+++ 02-Variables/variables.go
@@ -1,8 +1,39 @@
 package main
 
-import ("fmt")
+import "fmt"
+
+const LoginToken string = "dsfgjwgf" // public
 
 func main() {
-	var a,b,c,d int = 1,2,3,4
-	fmt.Println(a,b,c,d)
+	var username string = "Abhijeet"
+	fmt.Println(username)
+	fmt.Printf("Variable is of type: %T \n", username)
+
+	var isLoggedIn bool = false
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is of type: %T \n", isLoggedIn)
+
+	var smallVal int = 256
+	fmt.Println(smallVal)
+	fmt.Printf("Variable is of type: %T \n", smallVal)
+
+	var smallFloat float64 = 255.35465656546546
+	fmt.Println(smallFloat)
+	fmt.Printf("Variable is of type: %T \n", smallFloat)
+
+	// default values and some aliases
+	var anotherVariable int
+	fmt.Println(anotherVariable)
+	fmt.Printf("Variable is of type: %T \n", anotherVariable)
+
+	// implicit type
+	var website = "hello world"
+	fmt.Println(website)
+
+	// no var style
+	numberofUSers := 30000.0
+	fmt.Println(numberofUSers)
+
+	fmt.Println(LoginToken)
+	fmt.Printf("Variable is of type: %T \n", LoginToken)
 }
