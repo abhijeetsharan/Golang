ebad921d5f70defbb76816f9bcc29f9cdb7e49bb
Abhijeet Sharan <abhijeetsharan0007@gmail.com>
2025-02-04 23:46:45 +0530

    Add initial Go module and main program for time display functionality

---
+++ 06-MyTime/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Welcome to time study pf golang")
+
+	presentTime := time.Now()
+	fmt.Println("Current Time is: ", presentTime)
+
+	fmt.Println(presentTime.Format("01-02-2006 15:04:05 Monday"))
+
+	createdDate := time.Date(2020, time.October, 15, 23, 23, 0, 0, time.UTC)
+	fmt.Println("Created Date is: ", createdDate)
+	fmt.Println(createdDate.Format("01-02-2006 Monday"))
+}
