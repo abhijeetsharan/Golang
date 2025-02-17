4185176671cc9d9a8cfaca79f44d7f5494327437
Abhijeet Sharan <abhijeetsharan0007@gmail.com>
2025-02-17 18:38:12 +0530

    Add initial implementation for web verbs with GET and POST requests

---
+++ 21-WebReqVerbs/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Welcome to the Web Verbs")
+	// PerformGetRequest()
+	// PerformPostRequest()
+	PerformPostFormRequest()
+}
+
+func PerformGetRequest(){
+	const myurl = "https://jsonplaceholder.typicode.com/todos/1"
+
+	response, err := http.Get(myurl)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+	fmt.Println("Status Code: ", response.StatusCode)
+	fmt.Println("Content Length is: ", response.ContentLength)
+
+	var responseString strings.Builder
+	content, _ := io.ReadAll(response.Body)
+	byteCount, _ := responseString.Write(content)
+
+	fmt.Println("Bytecount is: ", byteCount)
+	fmt.Println(responseString.String())
+
+	// fmt.Println("Content is: ", string(content))
+}
+
+func PerformPostRequest(){
+	const myurl = "http://localhost:8000/post"
+
+	// fale json payload
+	requestBody := strings.NewReader(`
+		{
+			"coursename": "Lets go with golang",
+			"price" : 0,
+			"platform": "Udemy"		
+		}
+	`)
+
+	response, err := http.Post(myurl, "application/json", requestBody)
+
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+	content, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(content))
+}
+
+func PerformPostFormRequest(){
+	const myurl = "http://localhost:8000/postform"
+
+	// formdata
+
+	data := url.Values{}
+	data.Add("firstname", "Abhijeet")
+	data.Add("lastname", "Sharan") 
+	data.Add("email", "abhijeet@go.dev") 
+
+	response, err := http.PostForm(myurl, data)
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	content, _ := io.ReadAll(response.Body)
+	fmt.Println(string(content))
+
+}
