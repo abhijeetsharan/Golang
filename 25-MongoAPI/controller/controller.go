bcaeccec0a56a835e191e70e7305ecff2d33d421
Abhijeet Sharan <abhijeetsharan0007@gmail.com>
2025-02-28 12:39:54 +0530

    Add MongoDB helper functions for movie insertion and update

--- 25-MongoAPI/controller/controller.go
+++ 25-MongoAPI/controller/controller.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/abhijeetsharan/mongoapi/model"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
@@ -33,4 +36,28 @@ func init() {
 
 	//collection instance
 	fmt.Println("Collection instance created!")
+}
+
+// MONGODB helpers - file
+
+// insert 1 record
+func insertOneMovie(movie model.Netflix){
+	inserted, err := collection.InsertOne(context.Background(), movie)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted one movie: ", inserted.InsertedID)
+}
+
+// update 1 record
+func updateOneMovie(movieId string){
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"watched": true}}
+
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Updated one movie: ", result.ModifiedCount)
 }
